43057ad640309fff0004f9b5607b1e6c87bd70df
Fynn Mollenhauer <fynn@live.de>
2019-12-08 20:06:11 +0000

    Made Camera and added gravity to flying contraption

---
+++ Flight of the Icarus/Assets/Scripts/FlyingSection/FlyingContraption/ContraptionController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContraptionController : MonoBehaviour
+{
+    public float gravityMultiplier;
+
+
+    void Start()
+    {
+        gravityMultiplier = 2.0f;
+
+    }
+
+
+    void FixedUpdate()
+    {
+        if (gameObject.GetComponent<Rigidbody>().velocity.magnitude <= gravityMultiplier)
+        {
+            gameObject.GetComponent<Rigidbody>().AddRelativeForce(Vector3.down * gravityMultiplier);
+        }
+
+        Debug.Log(gameObject.transform.position.y);
+    }
+}
