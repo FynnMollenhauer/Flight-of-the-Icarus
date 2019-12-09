6e1364efeda905bf7e9d95b533babccc63d45a35
Fynn Mollenhauer <fynn@live.de>
2019-12-09 00:28:47 +0000

    Finished the character controller for the flying section

---
+++ Flight of the Icarus/Assets/Scripts/Helpers/InputHelper.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public static class InputHelper
+{
+    public static float GetSteering()
+    {
+        return Input.GetAxis("Horizontal");
+    }
+
+    public static float GetPanning()
+    {
+        return Input.GetAxis("Vertical");
+    }
+
+    public static bool IsSteering()
+    {
+        return GetSteering() != 0.0f;
+    }
+
+    public static bool IsPanning()
+    {
+        return GetPanning() != 0.0f;
+    }
+}
