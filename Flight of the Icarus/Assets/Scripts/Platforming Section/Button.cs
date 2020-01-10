d064fea0ae7dca8610d50ce8cfd99d54f9075bc4
Fynn Mollenhauer <fynn@live.de>
2020-01-10 18:14:04 +0000

    Finished putting the buttons in, but did some stuff wrong..will correct tomorrow

--- Flight of the Icarus/Assets/Scripts/Platforming Section/Button.cs
+++ Flight of the Icarus/Assets/Scripts/Platforming Section/Button.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class Button : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public bool isReadyForInput;
+    public bool wasUsed;
+
+    public GameObject target;
+
+    private void OnCollisionEnter(Collision collision)
     {
-        
+        if (collision.gameObject.tag != "Player")
+            return;
+
+        isReadyForInput = true;
     }
 
-    // Update is called once per frame
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag != "Player")
+            return;
+
+        isReadyForInput = false;
+    }
+
     void Update()
     {
-        
+        if (isReadyForInput == true && wasUsed == false && Input.GetKey(KeyCode.E))
+        {
+            target.SetActive(true);
+            wasUsed = true;
+            gameObject.transform.Translate(new Vector3(0, 0.4f, 0));
+        }
+
     }
 }
